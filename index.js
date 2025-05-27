8f456993be3a2d7497bed351ee71c56d74fecad8
Dipok Kumar Ray <dipok7316@gmail.com>
2025-05-27 10:48:48 +0600

    added axios used

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 3000;
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //user : cofee-monster
 //pass : wEbTpXvnC9sKyVb4
 // console.log(process.env.BD_USER);
 // console.log(process.env.BD_PASS);
 
-
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.oclat4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log('Mongo URI:', uri); 
+console.log("Mongo URI:", uri);
 
 // const uri = `mongodb+srv://cofee-monster:wEbTpXvnC9sKyVb4@cluster0.oclat4d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,98 +25,100 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    
-    const coffeesCollection = client.db('coffeeDB').collection('coffees');   
-    const usersCollection = client.db('coffeeDB').collection('users');
 
+    const coffeesCollection = client.db("coffeeDB").collection("coffees");
+    const usersCollection = client.db("coffeeDB").collection("users");
 
     //Read kora hoyeche
-    app.get('/coffees', async(req, res) => {
-        // const cursor = coffeesCollection.find();
-        // const result = await cursor.toArray();
-        //
-        const result = await coffeesCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/coffees", async (req, res) => {
+      // const cursor = coffeesCollection.find();
+      // const result = await cursor.toArray();
+      //
+      const result = await coffeesCollection.find().toArray();
+      res.send(result);
+    });
 
-
-
-    app.get('/coffees/:id', async(req, res) => {
+    app.get("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await coffeesCollection.findOne(query);
       res.send(result);
-    })
-
+    });
 
     //post mane tw insert kora hoyeche
-    app.post('/coffees', async(req, res) => {
+    app.post("/coffees", async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeesCollection.insertOne(newCoffee);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     //ekhane update korano hoyeche
-    app.put('/coffees/:id' , async(req, res) => {
+    app.put("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updatedCoffee = req.body;
-      const options = { upset : true};
+      const options = { upset: true };
       const updateDoc = {
-        $set:updatedCoffee
-      }
-      const result = await coffeesCollection.updateOne(filter, updateDoc, options);
-      res.send(result)
-    })
+        $set: updatedCoffee,
+      };
+      const result = await coffeesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
     //one or two data patching korano hoyeche
-    app.patch('/users', async(req, res) => {
-      const {email, lastSignInTime} = req.body;
-      const filter = {email: email }
+    app.patch("/user", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
       const updatedDoc = {
         $set: {
-          lastSignInTime: lastSignInTime
-        }
-      }
+          lastSignInTime: lastSignInTime,
+        },
+      };
 
-      const result = await usersCollection.updateOne(filter, updatedDoc)
+      const result = await usersCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
+    });
 
-
-    //data delete korano hoyeche 
-    app.delete('/coffees/:id', async(req, res) => {
+    //data delete korano hoyeche
+    app.delete("/coffees/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const result  = await coffeesCollection.deleteOne(query);
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeesCollection.deleteOne(query);
       res.send(result);
-    })
-
+    });
 
     //User related APIs
-    app.post('/users', async(req, res) => {
+    app.post("/user", async (req, res) => {
       const userProfile = req.body;
       console.log(userProfile);
       const result = await usersCollection.insertOne(userProfile);
       res.send(result);
-    })
+    });
 
-
+    // Get all users
+    app.get("/user", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -129,14 +126,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Coffe server is getting hotter')
-})
-
+app.get("/", (req, res) => {
+  res.send("Coffe server is getting hotter");
+});
 
 app.listen(port, () => {
-    console.log(`Coffe server is running on port : ${port}`);
-})
+  console.log(`Coffe server is running on port : ${port}`);
+});
